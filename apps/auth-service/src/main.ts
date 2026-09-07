7b767bd6399dbbab24e22f77bd76a65d8936821d
Mithun Yadav <mithuny1998@gmail.com>
2026-09-07 23:28:40 +0530

    feat: implement the swagger configuration

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -5,9 +5,11 @@ import {ConfigService} from '@nestjs/config';
 import {Logger} from 'nestjs-pino';
 import {HttpExceptionFilter} from './common/filters/http-exception.filter';
 import helmet from 'helmet';
+import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+    const configService = app.get(ConfigService);
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -21,7 +23,15 @@ async function bootstrap() {
   app.useLogger(app.get(Logger));
   app.use(helmet());
 
-  const configService = app.get(ConfigService);
+  if(configService.get<string>('NODE_ENV') === 'development'){
+    const config = new DocumentBuilder().setTitle('HyperThread Auth service').setDescription('API documentation for the HyperThread Auth service')
+    .setVersion('1.0')
+    .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+
+    SwaggerModule.setup('docs', app, document)
+  }
    app.enableCors({
     origin: configService.get<string>('FRONTEND_URL'),
     credentials: true
